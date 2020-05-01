138162cdd6f2d7d581432219ccb6e0dda2283395
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2020-05-01 10:10:32 +0200

    uopdate favicon

---
+++ build/precache-manifest.a0fb1a1a467479bbaecf8dbfeee86e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6532e646255393b9d591c96e1dfa154a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9526a1b7250c53977ebc",
+    "url": "/static/css/main.a6fb5ddc.chunk.css"
+  },
+  {
+    "revision": "4aaeb5177d8a4bc491e8",
+    "url": "/static/js/2.a6d7d059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6d7d059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9526a1b7250c53977ebc",
+    "url": "/static/js/main.0c6e4f0c.chunk.js"
+  },
+  {
+    "revision": "481a878b66ec9005e5a7",
+    "url": "/static/js/runtime-main.eab7bb29.js"
+  }
+]);
--- src/contentful.js
+++ src/contentful.js
@@ -2,7 +2,6 @@ const contentful = require("contentful");
 
 const spaceKey = process.env.REACT_APP_SPACE_KEY;
 const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
-console.log(spaceKey);
 
 const client = contentful.createClient({
   space: spaceKey,
